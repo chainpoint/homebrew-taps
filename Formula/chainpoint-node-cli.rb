b28e19b83acb77a79cf322c5555bed111838953a
Glenn Rempe <glenn@tierion.com>
2018-10-01 19:51:39 -0700

    Bump version to 0.2.6

--- Formula/chainpoint-node-cli.rb
+++ Formula/chainpoint-node-cli.rb
@@ -1,9 +1,9 @@
 class ChainpointNodeCli < Formula
   desc "Command-line Interface (CLI) for managing Chainpoint Nodes"
   homepage "https://github.com/chainpoint/chainpoint-node-cli"
-  url "https://github.com/chainpoint/chainpoint-node-cli/releases/download/v0.2.5/chainpoint-node-cli-macos-x64"
-  version "0.2.5"
-  sha256 "3fed4e836e5b93ddde95d2237bae451f7052a724383d90973360cf65581d8b04"
+  url "https://github.com/chainpoint/chainpoint-node-cli/releases/download/v0.2.6/chainpoint-node-cli-macos-x64"
+  version "0.2.6"
+  sha256 "71a255e8ac1cabf68caaac0d8e1b17ee8fc8dc17a446021462b5b1cd12c4244f"
 
   bottle :unneeded
 
