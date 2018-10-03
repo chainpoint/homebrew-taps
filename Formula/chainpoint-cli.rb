d44118b0fcdc7e1fa874aee9b52b1672b479ce6b
Glenn Rempe <glenn@tierion.com>
2018-10-03 10:13:23 -0700

    Bump chainpoint-cli to v1.4.7

--- Formula/chainpoint-cli.rb
+++ Formula/chainpoint-cli.rb
@@ -1,9 +1,9 @@
 class ChainpointCli < Formula
   desc "Command-line Interface (CLI) for managing timestamp proofs"
   homepage "https://github.com/chainpoint/chainpoint-cli"
-  url "https://github.com/chainpoint/chainpoint-cli/releases/download/v1.4.6/chainpoint-cli-macos-x64"
-  version "1.4.6"
-  sha256 "4fa362c5830cbb93deca8732ae4940374634ad4d3cbda1086eec164613fcdb3e"
+  url "https://github.com/chainpoint/chainpoint-cli/releases/download/v1.4.7/chainpoint-cli-macos-x64"
+  version "1.4.7"
+  sha256 "a83fab7afe87e46b49cab8ead5f9eb8a4608183b7e61d0e7e9dbc53c3c6042c9"
 
   bottle :unneeded
 
